3a5605163fe0effa79e2ba5214b08ca96a65600d
rajeshVC <rajeshkumar.d@cybernetyx.com>
2023-07-20 12:56:29 +0530

    refactored to have the mapping from OV (@480p) to 4K (@720p) and removed unwanted directories

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@
 #define N_THREADS 2
 #define CAMERA_FOV 140
 
+#define STARTX 174
+#define ENDX 623
+#define STARTY 110
+#define ENDY 362
+
+#define SECONDARY_CAMERA 4
+#define PRIMARY_CAMERA 2
+
 using namespace std;
 using namespace cv;
 
@@ -20,6 +28,94 @@ struct FOV{
     int max = 180 - min;
 } FOV;
 
+struct BoundingBox {
+    float x1, x2, y1, y2;
+} Bbox;
+
+void drawBoundingBoxOnPrimaryDevice(const cv::Mat& image, const BoundingBox& box, const std::vector<FaceInfo>& finalBox) {
+    
+    VideoCapture capPrimary(PRIMARY_CAMERA);
+    if(!capPrimary.isOpened()) {
+        std::cout<<"Failed to open the camera"<<std::endl;
+        exit(1);
+    }
+    cv::Mat framePrimary;
+    while(1) {
+        capPrimary.read(framePrimary);
+        if (framePrimary.empty()) {
+            cerr << "ERROR: Unable to grab from the camera" << endl;
+            break;
+        }
+
+        cv::Rect bboxPrimary = cv::Rect(box.x1, box.y1, (box.x2-box.x1), (box.y2-box.y1));
+        cv::rectangle(image, bboxPrimary, cv::Scalar(255, 255, 255), 2);
+        putText(image, std::to_string(finalBox.size()),Point(10,15),FONT_HERSHEY_SIMPLEX,0.6, Scalar(255, 0, 0), 2);
+
+        String winName = "[PRIMARY CAMERA]";
+        namedWindow(winName, cv::WINDOW_NORMAL);
+        imshow(winName, image);
+
+
+        char esc = waitKey(5);
+        if(esc == 27) break;
+    }
+
+    capPrimary.release();
+    cv::destroyAllWindows();
+
+}
+
+// int getPan(const cv::Mat& image, BoundingBox box){
+//     int pan = -1;
+//     if(bboxCentreX < image.cols/2) {
+//         pan = - (eachPixelWorthPan * bboxCentreX);
+        
+//     } else {
+//         pan = eachPixelWorthPan * bboxCentreX;
+        
+//     }
+    
+//     return pan;
+// }
+
+// int getTilt(const cv::Mat& image, BoundingBox box){
+//     int tilt = -1;
+//     if(bboxCentreX < image.cols/2) {
+//         tilt = - (eachPixelWorthTilt * bboxCentreY);
+        
+//     } else {
+//         tilt = eachPixelWorthTilt * bboxCentreY;
+        
+//     }
+//     return tilt;
+// }
+
+// int getZoom(const cv::Mat& image, BoundingBox box1){
+//     int zoom = -1;
+
+//     int deltaBBox = 
+// }
+
+// void setPTZ(BoundingBox box, cv::Mat image) {
+//     float bboxCentreX = (box.x2 - box.x1)/2;
+//     float bboxCentreY = (box.y2 - box.y1)/2;
+
+//     //!36000 supposed to be replaced by PanMax/TiltMax
+//     int eachPixelWorthPan = 36000/(image.cols/2);
+//     int eachPixelWorthTilt = 36000/(image.rows/2);
+
+//     int pan = -1, tilt = -1, zoom = -1;
+//     if(bboxCentreX < image.cols/2) {
+//         pan = - (eachPixelWorthPan * bboxCentreX);
+
+//     } else {
+//         pan = eachPixelWorthPan * bboxCentreX;
+        
+//     }
+
+
+// }
+
 //!for micDoA integration
 int micDoASimulator() {
     std::random_device rd;
@@ -29,11 +125,11 @@ int micDoASimulator() {
     return lk(mt);
 }
 
-//! to identify the speaker and locate position/bbox
-//!for micDoA integration
+// //! to identify the speaker and locate position/bbox
+// //!for micDoA integration
 FaceInfo identifyTheSpeaker(const cv::Mat& image, const std::vector<FaceInfo>& faceList) {
     int angle = micDoASimulator();
-    float nthPixelPerDegree = image.rows / 140.0;
+    float nthPixelPerDegree = image.rows / 160.0;
 
     if(angle < FOV.min) {
         angle = FOV.min;
@@ -47,7 +143,7 @@ FaceInfo identifyTheSpeaker(const cv::Mat& image, const std::vector<FaceInfo>& f
     
 
     FaceInfo currentSpeaker = {};
-    for(auto i=0; i<faceList.size(); i++){
+    for(auto i=0; i<faceList.size(); i++) {
         if(currentPixel < (faceList[i].x1 + faceList[i+1].x1)/2) {
             currentSpeaker = faceList[i];
             break;
@@ -70,8 +166,9 @@ void resetToAspectRatio(float& width, float& height) {
     float multiple_16 = width/16.0;
 
     float multiple = std::max(multiple_16, multiple_9);
-    width = multiple * 16;
-    height = multiple * 9;
+    
+    width = (multiple + 2) * 16;
+    height = (multiple + 8) * 9;
 
     // std::cout<<"new aspect ratio: "<<width<<" x "<<height<<std::endl; 
     
@@ -79,17 +176,13 @@ void resetToAspectRatio(float& width, float& height) {
 
 void drawBiggerBoundingBox(const std::vector<FaceInfo>& in, const cv::Mat& resized, int type=0){
     
-    float x1_min = in[0].x1, y1_min = in[0].y1, x2_max = in[0].x2, y2_max = in[0].y2;
-
-    std::cout<<"FaceList with the respective WxH: "<<std::endl;
-    std::cout<<"-------------------------------------------"<<std::endl;
-    for(const auto& ch : in){
-        std::cout<<"x1: "<<ch.x1<<" -- x2: "<<ch.x2<<std::endl;
-        std::cout<<"y1: "<<ch.y1<<" -- y2: "<<ch.y2<<std::endl;
+    if(in.empty()){
+        return;
     }
 
-    std::cout<<"----------------------------------------"<<std::endl;
-    
+    float x1_min = in[0].x1, y1_min = in[0].y1, x2_max = in[0].x2, y2_max = in[0].y2;
+
+  
     for(auto i=0; i<in.size(); i++) {
         x1_min = std::min(x1_min, in[i].x1);
         y1_min = std::min(y1_min, in[i].y1);
@@ -104,22 +197,13 @@ void drawBiggerBoundingBox(const std::vector<FaceInfo>& in, const cv::Mat& resiz
     
     float bbox_w = occupied_width;
     float bbox_h = occupied_height;
-    // std::cout<<"old aspect ratio: "<<occupied_width<<" x "<<occupied_height<<std::endl;
     
     resetToAspectRatio(bbox_w, bbox_h);
 
-    if(in.size() == 1) {
-        bbox_w *= 2.3;
-        bbox_h *= 2.3;
-        
-    }
-
     float new_x1 = x1_min == 0 ? 0 : x1_min - ((bbox_w - occupied_width)/2);
     float new_y1 = y1_min == 0 ? 0 : y1_min - ((bbox_h - occupied_height)/2);
-    
-    if(in.size() == 1){
-        new_y1 += (y1_min - new_y1) / 2;
-    }
+    new_y1 += (y1_min - new_y1) / 2;
+ 
    
     cv::Rect bbox = cv::Rect(new_x1, new_y1, bbox_w, bbox_h);
     cv::rectangle(resized, bbox, cv::Scalar(0, 0, 0), 2);
@@ -141,7 +225,7 @@ int main(int argc, char **argv)
 
     FaceDetection *ultra = new FaceDetection(std::stoi(argv[2]));
 
-    VideoCapture cap(2);
+    VideoCapture cap(SECONDARY_CAMERA);
     // VideoCapture cap("../resources/videos/Walks2.mp4");
     if (!cap.isOpened()) {
         cerr << "ERROR: Unable to open the video" << endl;
@@ -151,6 +235,7 @@ int main(int argc, char **argv)
     std::cout << "Start grabbing, press ESC on Live window to terminate" << endl;
 
     int i = 0;
+    
 
     while(1){
         cap >> frame;
@@ -160,22 +245,54 @@ int main(int argc, char **argv)
         }
 
         //resize the frame
-        cv::Mat resized;
-        cv::resize(frame, resized, cv::Size(320, 240));
 
-        // int64 start = cv::getTickCount();
+        //resize using the empherical starX startY endX endY
 
+
+        int kwidth = ENDX- STARTX;
+        int kheight = ENDY - STARTY;
+
+        //read the frame from remote side and check the reoslution it is opened in - 1080p (16:9)
+        // cv::resize(frame, resized, cv::Size(1920, 1080));
+        cv::Rect myROI(STARTX,STARTY,kwidth,kheight);
+        cv::Mat resized = frame(myROI);
+    
         std::vector<FaceInfo> finalBox;
         if(i % std::stoi(argv[1]) == 0) {
             
             ultra->detect(resized,finalBox,resized.rows,resized.cols);
 
+
             std::cout<<"Face list: "<<finalBox.size()<<std::endl;
+
+            std::cout<<"FaceList with the respective WxH: "<<std::endl;
+            std::cout<<"-------------------------------------------"<<std::endl;
+            for(const auto& ch : finalBox){
+                std::cout<<"x1: "<<ch.x1<<" -- x2: "<<ch.x2<<std::endl;
+                std::cout<<"y1: "<<ch.y1<<" -- y2: "<<ch.y2<<std::endl;
+            }
+
+            std::cout<<"----------------------------------------"<<std::endl;
+            std::cout<<"----------------------------------------"<<std::endl;
+        //!map this small image to actual image
+        float ovX1, ovY1, ovX2, ovY2;
             for(size_t i = 0; i < finalBox.size(); i++) {
                 FaceInfo facebox = finalBox[i];
                 float width  = facebox.x2-facebox.x1;
                 float height = facebox.y2-facebox.y1;
-                cv::Rect box=cv::Rect(facebox.x1, facebox.y1, width, height);
+                
+                BoundingBox bb;
+                
+                bb.x1 = facebox.x1;
+                bb.x2 = facebox.x2;
+                bb.y1 = facebox.y1;
+                bb.y2 = facebox.y2;
+                cv::Rect box=cv::Rect(bb.x1, bb.y1, (bb.x2-bb.x1), (bb.y2-bb.y1));
+                
+                
+
+                std::cout<<"ovX1: "<<ovX1 <<" --- ovX2: "<<ovX2<<std::endl;
+                std::cout<<"ovY1: "<<ovY1 <<" --- ovY2: "<<ovY2<<std::endl;
 
                 if(argv[3]) {
                     std::vector<FaceInfo> locatedSpeaker;
@@ -185,6 +302,7 @@ int main(int argc, char **argv)
 
                 } else {
                     drawBiggerBoundingBox(finalBox, resized);
+                // drawBoundingBoxOnPrimaryDevice(resized, bb, finalBox);
 
                 }
 
@@ -200,7 +318,7 @@ int main(int argc, char **argv)
         // double fps = cv::getTickFrequency() / (cv::getTickCount() - start);
 
         //show output
-        String winName = "Face Detection";
+        String winName = "[SECONDARY CAMERA]";
         namedWindow(winName, cv::WINDOW_NORMAL);
         imshow(winName, resized);
 
